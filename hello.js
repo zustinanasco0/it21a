265419cc715add1d77a52086d05f9cc0a73e0de8
Zustin Mervin Paul A. Añasco <zustinanasco0@gmail.com>
2025-08-28 08:47:38 +0800

    Added Substraction, multiplication and division

--- hello.js
+++ hello.js
@@ -1,9 +1,16 @@
 console.log("Hello World") ; 
 
+
 num1 = 2;
-num2 = 3;   
+num2 = 3;
+
 
 sum = num1 + num2;
+diff = num1 - num2;
+prod = num1 * num2;
+quo = num1 / num2;
 
-
-console.log(sum) ;
+console.log("The sum of " + num1 + " & " + num2 + " is " + sum);
+console.log("The difference of " + num1 + " & " + num2 + " is " + diff);
+console.log("The product of " + num1 + " & " + num2 + " is " + prod);
+console.log("The quotient of " + num1 + " & " + num2 + " is " + quo);
