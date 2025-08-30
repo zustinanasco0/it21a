6c7920bff84b2a6ce19876442dff5ead0c38ecea
Zustin Mervin Paul A. Añasco <zustinanasco0@gmail.com>
2025-08-31 07:30:23 +0800

    Initial commit

--- hello.js
+++ hello.js
@@ -14,3 +14,4 @@ console.log("The sum of " + num1 + " & " + num2 + " is " + sum);
 console.log("The difference of " + num1 + " & " + num2 + " is " + diff);
 console.log("The product of " + num1 + " & " + num2 + " is " + prod);
 console.log("The quotient of " + num1 + " & " + num2 + " is " + quo);
+ console.log("Zustin Gwapo") ;
