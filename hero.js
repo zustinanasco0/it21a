3cfe029599d18ef3c39ed986294ecdd56e692c5e
KYLLETAAL09 <kylazyrataal@gmail.com>
2025-09-15 08:30:39 +0800

    Added totang talong(Mage)

--- hero.js
+++ hero.js
@@ -23,13 +23,22 @@ class Warrior extends Hero {
         console.log(`${this.name} uses a powerful strike!`) ;
 }
 
+}
 
+class Mage extends Hero {
+    constructor(name, health, attack, mana) {
+        super(name, health, attack) ;
+        this.mana = mana ;
+}
+   useAbility() {
+    console.log(`${this.name} casts a flying talong attack!`) ;
+
+}
 
 }
 
 const warrior = new Warrior("janloyd", 120, 15) ;
-
-
 warrior.useAbility() ;
 
-
+const mage = new Mage("totang talong", 80, 8, 100) ;
+mage.useAbility() ;
