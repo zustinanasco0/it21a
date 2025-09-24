36a4d214dd26b8676397964533fd7ef417800de2
zustin anasco <zustinanasco0@gmail.com>
2025-09-24 19:43:59 +0800

    it finally works lesgooo

--- person.js
+++ person.js
@@ -1,5 +1,5 @@
 class person{
-  comstructor(name,age,occupation){
+  constructor(name,age,occupation){
   this.name = name;
    this.age = age ;
    this.occupation = occupation ;
@@ -26,9 +26,13 @@ displayinfo() {
 }
 
 const person1 = new person("Coco Martin " ,24, "Masahista") ;
-
+const person2 = new person("Liza Soberano", 22, "Actress") ;
+const person3 = new person("Alden Richards", 30, "Actor") ;
  
 
 person1.getName() ;
 person1.displayinfo(); 
- 
+person2.getName() ;
+person2.displayinfo(); 
+person3.getName() ;
+person3.displayinfo();
