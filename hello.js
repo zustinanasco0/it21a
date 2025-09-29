a4f4233448be12d25096ecbc7ad1e82a309ee9db
ZustinAnasco <zustinanasco0@gmail.com>
2025-09-30 01:47:12 +0800

    Added basic syntax

--- hello.js
+++ hello.js
@@ -1,18 +1,107 @@
-console.log("Hello World") ; 
+// console.log("Hello World") ; 
 
 
-num1 = 2;
-num2 = 3;
-let X = "zustin gwapo"
+// num1 = 2;
+// num2 = 3;
+// let X = "zustin gwapo"
 
-sum = num1 + num2;
-diff = num1 - num2;
-prod = num1 * num2;
-quo = num1 / num2;
+// sum = num1 + num2;
+// diff = num1 - num2;
+// prod = num1 * num2;
+// quo = num1 / num2;
 
-console.log("The sum of " + num1 + " & " + num2 + " is " + sum);
-console.log("The difference of " + num1 + " & " + num2 + " is " + diff);
-console.log("The product of " + num1 + " & " + num2 + " is " + prod);
-console.log("The quotient of " + num1 + " & " + num2 + " is " + quo);
- console.log(X) 
- console.log("This is the Lab/factorial") ;
+// console.log("The sum of " + num1 + " & " + num2 + " is " + sum);
+// console.log("The difference of " + num1 + " & " + num2 + " is " + diff);
+// console.log("The product of " + num1 + " & " + num2 + " is " + prod);
+// console.log("The quotient of " + num1 + " & " + num2 + " is " + quo);
+//  console.log(X) 
+//  console.log("This is the Lab/factorial") ;
+
+
+
+
+// _________________________________________________________________________
+
+
+
+// FUnction 
+
+// function greet() {
+
+//   console.log("hilo niggas") ;
+
+// }
+
+// greet();
+
+
+// Function with Parameter
+
+// function  greet(name) {
+
+// console.log("Hello "+ name + " welcome to the program!") ;
+
+
+// }
+
+// greet("ambot") ;
+
+
+
+// function greet(name) {
+//     console.log("Hello "+ name + " welcome to the program!") ;
+// }
+
+// greet("ambot") ;
+
+
+
+// Function with Return Value
+
+// function add(num1, num2) {
+//     return num1 + num2;
+// }           
+
+// let sum = add(5, 10) ;
+// console.log("The sum is: " + sum) ;     
+
+
+// Function Arrow
+
+// const multiply = (x, y) => x * y;
+
+// console.log(multiply(4, 6)); // Output: 24
+
+
+// __________________________________________________________________________
+
+
+// Variables
+// let name = "Ronie";
+// const age = 52;
+
+// // Functions
+// function greet(user) {
+//   return "Hello, " + user + "!";
+// }
+
+// Conditional Statement
+
+
+// let name = "Ronie";
+// const age = 52;
+
+
+// if (age > 18) {
+//   console.log(name + " is an adult.");
+// } else {
+//   console.log(name + " is a minor.");
+// }
+
+// Loop
+// for (let i = 1; i <= 3; i++) {
+//   console.log("Count: " + i);
+// }
+
+// Using the function
+// console.log(greet(name));
