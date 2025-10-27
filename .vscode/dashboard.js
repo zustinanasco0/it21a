83e4907ac9d2c5e3baf2caef4e92ab54e4e1d2ac
Zustinanasco <zustinanasco0@gmail.com>
2025-10-27 08:36:19 +0800

    Added final touches

--- .vscode/dashboard.js
+++ .vscode/dashboard.js
@@ -1,3 +1,65 @@
+// 
+
+class LineChart{
+    constructor(canvasId,dataUrl){
+        this.canvasId = canvasId;
+        this.dataUrl = dataUrl;
+        this.chart = null;
+    }
+
+    renderChart(data){
+        const ctx = document.getElementById(this.canvasId).getContext("2d");
+
+        this.chart = new Chart(ctx,{
+            type:"line",
+            data:{
+                //Labels
+                labels: data.labels,
+                datasets:[{
+                    label:"Monthly Data",
+                    //Data
+                    data:data.values,
+                    borderwidth:1
+                }]
+            },
+            options:{
+                scales:{
+                    y:{
+                        beginAtZero:true
+                    }
+                }
+            }
+        });
+    }
+
+    async fetchData(){
+        try{
+            const response = await fetch(this.dataUrl);
+            
+            if(!response.ok) throw new Error(`Failed to load data: ${response.statusText}`);
+
+            const data = await response.json();
+            return data;
+
+        } catch(error){
+            console.error("Error fetching data:", error);
+            return null;
+        }
+    }
+
+    async init(){
+        const data = await this.fetchData();
+        if(data){
+            this.renderChart(data);
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    const chart = new LineChart("linechart","linedata.json");
+    chart.init();
+})
+
 class radarChart{
     constructor(canvasId,dataUrl){
         this.canvasId = canvasId;
@@ -11,9 +73,9 @@ class radarChart{
         this.chart = new Chart(ctx,{
             type:"radar",
             data:{
-              
+                //Labels
                 labels:data.labels,
-                
+                //Datasets
                 datasets: data.datasets
             },
             options:{
@@ -55,7 +117,7 @@ class radarChart{
         const data = await this.fetchData();
         if(data) this.renderChart(data);
     }
-}   
+}
 
 document.addEventListener("DOMContentLoaded",()=>{
     const chart = new radarChart("radarChart","radarData.json");
